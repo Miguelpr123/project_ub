5365533e48251c2c2bf4131ef75d9ecba0b799be
Miguelpr123 <87872201+Miguelpr123@users.noreply.github.com>
2022-06-05 04:52:36 +0200

    async req TO https://jsonplaceholder

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
 var req = new XMLHttpRequest();
-req.open('GET', 'http://www.mozilla.org/', true);
+req.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
 req.onreadystatechange = function (aEvt) {
   if (req.readyState == 4) {
      if(req.status == 200)
